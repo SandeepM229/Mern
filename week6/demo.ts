81778a30c2c247c8c89ca6ab8415dda36c95d349
Sandeep_229 <24B11AI229@adityauniversity.in>
2026-03-09 12:39:21 +0530

    Create demo.ts

---
+++ week6/demo.ts
@@ -0,0 +1,12 @@
+let message: string = "Hello Typescript"
+let num1: number = 10
+let num2: number = 20
+
+function add(num1: number, num2: number):number {
+    return num1+num2
+}
+
+add(num1, num2)
+console.log(message)
+
+export {}
